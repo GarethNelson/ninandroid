e9e2dd15c2032ad468dd7edceb4557407319bd1b
gareth@garethnelson.com <gareth@garethnelson.com@0704acd0-3922-11de-be05-5f7a86268029>
2009-05-05 16:13:00 +0000

    Bottom button bar

--- src/gen/com/nin/NinAndroid/R.java
+++ src/gen/com/nin/NinAndroid/R.java
@@ -13,9 +13,10 @@ public final class R {
     public static final class drawable {
         public static final int bottom_marquee=0x7f020000;
         public static final int default_av=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int login_btn=0x7f020003;
-        public static final int top_marquee=0x7f020004;
+        public static final int home_btn=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int login_btn=0x7f020004;
+        public static final int top_marquee=0x7f020005;
     }
     public static final class id {
         public static final int ImageButton01=0x7f050008;
