81e17b7fffa5eee5b0d98df43b89218a56900de0
gareth@garethnelson.com <gareth@garethnelson.com@0704acd0-3922-11de-be05-5f7a86268029>
2009-05-05 07:00:46 +0000

    first commit, basic skeleton

---
+++ trunk/src/src/com/nin/NinAndroid/NinAndroid.java
@@ -0,0 +1,33 @@
+package com.nin.NinAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class NinAndroid extends Activity {
+	private OnClickListener LoginClickListener = new OnClickListener() {
+	    public void onClick(View v) {
+	    	Toast.makeText(getApplicationContext(), "Login not yet implemented", Toast.LENGTH_SHORT).show();
+	    }
+	};
+
+	WebView webview;
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("file:///android_asset/home.html");
+
+        ImageView LoginButton = (ImageView)findViewById(R.id.LoginBtn);
+        LoginButton.setOnClickListener(LoginClickListener);
+
+    }
+}
